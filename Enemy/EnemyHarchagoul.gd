2a6cc67ffec8214aaa5924628e6ec80dceb6a0c2
Krynce <krynce.gs@gmail.com>
2023-06-20 12:50:30 +0300

    enemy aggretion to nearest player

--- Enemy/EnemyHarchagoul.gd
+++ Enemy/EnemyHarchagoul.gd
@@ -5,7 +5,6 @@ extends CharacterBody2D
 @export var max_hp = 10
 @export var hp_bar_fill_color = Color(190, 0, 0)
 
-@onready var player = get_tree().get_first_node_in_group("player")
 @onready var sprite = $sprite
 @onready var anim = $AnimationPlayer
 @onready var health_bar = $HealthBar
@@ -16,13 +15,15 @@ func _ready():
 	anim.play("walk")
 
 func _physics_process(_delta):
-	#var lst = Array()
-	#for player in get_tree().get_nodes_in_group("player"):
-	#	var b = player.global_position.distance_to(self.global_position)
-	#	var a = global_position.direction_to(player.global_position)
-	#	pass
-	var player = get_tree().get_first_node_in_group("player")
-	var direction = global_position.direction_to(player.global_position)
+	var nearest_player
+	var min_distance = 100000
+	var distance = 0
+	for player in get_tree().get_nodes_in_group("player"):
+		distance = self.global_position.distance_to(player.global_position)
+		if distance < min_distance:
+			min_distance = distance
+			nearest_player = player
+	var direction = global_position.direction_to(nearest_player.global_position)
 	velocity = direction * movement_speed
 	move_and_slide()
 
